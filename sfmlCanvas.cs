86c2d3feafaa2af9bec3299b7c2e551432e2127f
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2019-09-01 19:37:44 +0200

    new two events

--- sfmlCanvas.cs
+++ sfmlCanvas.cs
@@ -6,22 +6,33 @@ using SFML.System;
 
 namespace WindowsFormsApp
 {
-    public delegate void DrawEventHandler(RenderWindow window, Time elapsedTime);
+    public delegate void InitEventHandler(RenderWindow window);
+    public delegate void UpdateEventHandler(RenderWindow window, Time elapsedTime);
+    public delegate void DrawEventHandler(RenderWindow window);
     public partial class SfmlCanvas : UserControl
     {
         public RenderWindow RendWind;
+
+        public event InitEventHandler OnInit;
+        public event UpdateEventHandler OnUpdate;
         public event DrawEventHandler OnDraw;
 
-        protected virtual void Draw(Time elapsedTime)
+        protected virtual void Init()
         {
-            OnDraw?.Invoke(RendWind, elapsedTime);
+            OnInit?.Invoke(RendWind);
         }
-        
+        protected virtual void Update(Time elapsedTime)
+        {
+            OnUpdate?.Invoke(RendWind, elapsedTime);
+        }
+        protected virtual void Draw()
+        {
+            OnDraw?.Invoke(RendWind);
+        }
         public SfmlCanvas()
         {
             InitializeComponent();
         }
-
         private void SfmlCanvas_Load(object sender, EventArgs e)
         {
             if (!renderLoopWorker.IsBusy)
@@ -32,23 +43,23 @@ namespace WindowsFormsApp
         {
             RendWind = new RenderWindow((IntPtr)e.Argument);
             Clock clock = new Clock();
+            Init();
             while (RendWind.IsOpen)
             {
                 Time elapsed = clock.ElapsedTime;
                 clock.Restart();
                 RendWind.DispatchEvents();
                 RendWind.Clear(new Color(BackColor.R, BackColor.G, BackColor.B));
-                Draw(elapsed);
+                Update(elapsed);
+                Draw();
                 RendWind.Display();
             }
         }
-
         protected override void OnPaint(PaintEventArgs e)
         {
             if (RendWind == null)
                 base.OnPaint(e);
         }
-
         protected override void OnPaintBackground(PaintEventArgs pevent)
         {
             if (RendWind == null)
